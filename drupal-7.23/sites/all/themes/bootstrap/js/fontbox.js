f9f3249b7ed0e33bab73b828e1bd2e8cdcc79f8a
hieunh1989 <hieunh1989@gmail.com>
2013-09-24 16:20:52 +0700

    upload drupal

---
+++ drupal-7.23/sites/all/themes/bootstrap/js/fontbox.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+
+  Drupal.behaviors.exampleModule = {
+    attach: function (context, settings) {
+		
+		$('.dropdown-toggle').toggle(function(){
+			$('.dropdown-menu').show(300);
+		}, function(){
+			$('.dropdown-menu').hide(300);
+		})
+		
+    }
+  };
+
+})(jQuery);
